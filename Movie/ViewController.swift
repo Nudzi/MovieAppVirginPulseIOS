0c2699f95af39bf46c1c1419bfaabdacd74fafb3
Nudzi <nudzejma.kezo@edu.fit.ba>
2020-10-09 18:42:34 +0200

    Initial Commit

---
+++ Movie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie
+//
+//  Created by Nudzejma Kezo on 10/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
